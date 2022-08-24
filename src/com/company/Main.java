22238cbdecd10d3f70bd4f1133def1c2e5194408
William Duong <w1702@Williams-MacBook-Pro.local>
2022-08-24 13:23:44 +1000

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,124 @@
+package com.company;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //java -version
+
+        /* Hello world */
+        // what is the main() method signature? whats the alternative varargs version of it
+
+        // hello world, sout println vs print
+//        System.out.println("Hello World!");
+//
+//        System.out.print("asdasd");
+//        System.out.println("asdasd");
+//        System.out.println("hi");
+        // Sys out is basically never used in the real world unless using command line program, or for debugging. Usually a logging service is used instead
+
+
+        /* Data types */
+        // primitives
+//        int myint;
+//        double myFloat;
+//        char myChar;
+//        boolean myBoolean;
+//        // non primitives
+//        String myString;
+//        String[] myArray;
+//        Integer myInteger;
+        // enums and constants
+
+        /* comparisons */
+        // int and boolean equals, gt, lt
+        // String equals, not equals
+        // Ternary operators
+
+        /* basic data structures, list, different implementations of list, difference between list and array */
+//        List<String> names = new ArrayList<>(); // use in 90% of cases, fast read speed
+//        List<String> otherNames = new LinkedList<>(); // use for special cases only, very slow read speed but fast add/remove speed
+
+
+
+        /* control flow, while loop, for loop (for each vs regular index), switch, break */
+//        List<String> people = Arrays.asList("Andy", "Will");
+//        String[] pets = {"cat", "dog"};
+//
+//        System.out.println("Names list using for each loop:");
+//        for (String person : people) {
+//            System.out.println(person);
+//        }
+//
+//        System.out.println("Names list using for loop:");
+//        for (int i = 0; i < people.size(); i++) {
+//            System.out.println(people.get(i));
+//        }
+//
+//        System.out.println("Pets array using for loop, gets very clunky and unreadable:");
+//        for (int i = 0; i < pets.length; i++) {
+//            System.out.println(pets[i]);
+//        }
+        // when to use for loop vs while loop?
+        // when to use switch?
+        // when to use break?
+
+
+        /* break it down, basic pattern, sum, if any exercises and higher order functions, lambdas */
+        // sum the numbers / reduce
+//        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+
+        // Get the names starting with a vowel (a, e, i, o, u) / filter
+//        List<String> names = Arrays.asList("Alex", "Chris", "Electra", "Ian", "Oppa", "Upton", "Will");
+
+        // Apply an additional 20% increase to each rate / map
+//        List<Double> rates = Arrays.asList(0.1, 0.2, 0.3, 0.4, 0.5);
+
+
+        /* functions vs procedures */
+
+        /* access modifiers */
+
+        /* OOP - Abstraction, Encapsulation, Inheritance, Polymorphism (method overloading), inner classes, oop design */
+
+        /* packages */
+
+        /* advanced data structures list remove problem, hashmap, stack, queue, set, tree */
+
+        /* maven, dependencies and jars */
+
+        /* basic mvc pattern, sql, db abstraction */
+
+        /* Builder pattern */
+
+        /* DI */
+
+        /* unit testing */
+
+        /* ci */
+
+        /* spring */
+
+        /* calling apis */
+
+        /* validation and Exceptions */
+
+        /* File I/O */
+
+        /* git */
+
+        /* scrum and workflows */
+
+        /* project from scratch */
+
+        /* integratiom testing/ testing pyramid */
+
+        /* data structures and algorithms, nested loops, reusing objects, recursion */
+    }
+
+    enum TshirtSizes {
+        SMALL, MEDIUM, LARGE
+    }
+}
